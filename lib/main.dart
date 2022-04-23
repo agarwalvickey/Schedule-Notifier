74ea2ad87cca667f77de339ec2d1a45fcc0e8cdb
agarwalvickey <agarwalvickey87@gmail.com>
2022-04-23 10:04:30 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ import 'package:remind/task_manager.dart';
 import 'package:remind/task.dart';
 import 'database.dart';
 import 'package:intl/intl.dart';
-
+import 'package:weekday_selector/weekday_selector.dart';
+List<String> getday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Injection.initInjection();
@@ -39,9 +40,9 @@ class _homeState extends State<home> {
       _tasks=getElements();
     });
   }
-  callBack2(int hour, int minute,String title,String details){
+  callBack2(int id,int d,int hour, int minute,String title,String details){
 
-    notificationScheduled(hour,minute,title,details);
+    notificationScheduled(id,d,hour,minute,title,details);
   }
   FlutterLocalNotificationsPlugin flutterNotificationPlugin;
 
@@ -68,7 +69,7 @@ class _homeState extends State<home> {
         context: context,
         builder: (_) => AlertDialog(
           title: Text(
-              "Hello Everyone"
+              "Your Scheduled Event"
           ),
           content: Text(
               "$payload"
@@ -91,7 +92,9 @@ class _homeState extends State<home> {
                   AsyncSnapshot<List<Map<dynamic, dynamic>>> snapshot) {
                 if (!snapshot.hasData) {
                   return Center(
-                    child: CircularProgressIndicator(),
+                    child: Text(
+                      "You don't have any event scheduled yet, Please add an event"
+                    ),
                   );
                 } else {
 
@@ -100,19 +103,21 @@ class _homeState extends State<home> {
                     itemCount: snapshot.data.length,
                     itemBuilder: (context, index) {
                       return ListTile(
-                        leading: Icon(Icons.add),
+                        leading: Icon(Icons.add_alert),
                         title: Text(snapshot.data[index]['title'],textScaleFactor: 1.5,),
                         trailing: Row(
                           mainAxisSize: MainAxisSize.min,
                           children: [
                             Text(
-                              snapshot.data[index]['reminder_time']
+                              // snapshot.data[index]['reminder_time'][0]
+                                  getday[int.parse(snapshot.data[index]['reminder_time'].substring(0,1))-1]+"\n"+snapshot.data[index]['reminder_time'].substring(1)
                             ),
                             // Text(s)
 
-                            Icon(Icons.edit,color: Colors.grey,),
-                            IconButton(onPressed: (){
+                            // Icon(Icons.edit,color: Colors.grey,),
+                            IconButton(onPressed: () async {
                               deleteItem(snapshot.data[index]['id']);
+                              await flutterNotificationPlugin.cancel(snapshot.data[index]['id']);
                               callBack();
                             }, icon: const Icon(Icons.delete,color: Colors.red,)),
 
@@ -143,13 +148,13 @@ class _homeState extends State<home> {
       ),
     );
   }
-  Future<void> notificationScheduled(int hour,int minute,String title,String detail) async {
+  Future<void> notificationScheduled(int id,int d,int hour,int minute,String title,String detail) async {
     // int hour = 13;
     var ogValue = hour;
     // int minute = 46;
 
     var time = Time(hour,minute,0);
-
+    var day=Day(d);
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
       'repeatDailyAtTime channel id',
       'repeatDailyAtTime channel name',
@@ -165,8 +170,8 @@ class _homeState extends State<home> {
     var platformChannelSpecifics = NotificationDetails(
         android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
 
-    await flutterNotificationPlugin.showDailyAtTime(4,title,
-      detail,time, platformChannelSpecifics,payload: "Hello",);
+    await flutterNotificationPlugin.showWeeklyAtDayAndTime(id,title,
+      detail,day,time, platformChannelSpecifics,payload: title+" : " +detail);
 
     print('Set at '+time.minute.toString()+" +"+time.hour.toString());
 
@@ -188,6 +193,8 @@ class _formState extends State<form> {
   TextEditingController createdate = TextEditingController();
   TextEditingController reminderdate = TextEditingController();
   var departureDate;
+  final values = List.filled(7, false);
+  // values[1]=true;
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -211,33 +218,59 @@ class _formState extends State<form> {
               validator: (value) => (value == null || value.isEmpty)
                   ? 'Required Field'
                   : null,
-              type: DateTimePickerType.dateTime,
-              firstDate: DateTime(1900),
-              lastDate: DateTime(2100),
-              initialDate: DateTime.now(),
-              dateLabelText: 'Reminder Date',
+              type: DateTimePickerType.time,
+              initialTime: TimeOfDay.now() ,
+
+              // firstDate: DateTime(1900),
+              // lastDate: DateTime(2100),
+              // initialDate: DateTime.now(),
+              // dateLabelText: 'Reminder Date',
               timeLabelText: 'Reminder Time',
               onChanged: (value) {
-                departureDate = DateTime.parse(value);
+                // departureDate = DateTime.parse(value);
+                departureDate= value;
 
+                // departureDate=value.timeZoneOffset.inHours.toString();
               },
             ),),
             Container(
+                child: WeekdaySelector(
+                  onChanged: (int day) {
+                    setState(() {
+
+                      final index = day % 7;
+                      values[index] = !values[index];
+                    });
+                  },
+                  values: values,
+                )
+            ),
+            Container(
               child: ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   DateTime now = DateTime.now();
                   String formattedDate = DateFormat('EEE d MMM \n kk:mm').format(now);
-                  String reminderDate = DateFormat('EEE d MMM \n kk:mm').format(departureDate);
-                  inserttasks(details.text, title.text, reminderDate,
+                  // String reminderDate = DateFormat('kk:mm').format(departureDate);
+                  String reminderDate=departureDate;
+                  int x=7;
+                  for(int i=0;i<values.length;i++){
+                    if(values[i]){
+                      x=i;
+                      break;
+                    }
+                  }
+                  // List<String> temp=reminderDate.split("\n");
+                  // reminderDate=temp[1];
+                  String val=(x+1).toString()+reminderDate;
+                  int id=await inserttasks(details.text, title.text, val,
                      formattedDate );
-                  List<String> temp=reminderDate.split("\n");
-                  String temp2=temp[1];
+                  String temp2=reminderDate;
                   List<String> temp3=temp2.split(":");
                   int hour=int.parse(temp3[0]);
                   int minute=int.parse(temp3[1]);
                   print(hour);
                   print(minute);
-                  widget.callback2(hour,minute,title.text,details.text);
+                  widget.callback2(id,x+1,hour,minute,title.text,details.text);
                   widget.callback();
                   Navigator.pop(context);
                 },
